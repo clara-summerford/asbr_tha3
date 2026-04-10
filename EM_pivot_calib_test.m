ff6451872a0f0ea38d7affd9d3c834556c6a6ff2
Nathan Lovell <nathanplovell@gmail.com>
2026-04-10 16:29:14 -0500

    Finished pivot calibration test functions. Added an error plotting code as well

--- EM_pivot_calib_test.m
+++ EM_pivot_calib_test.m
@@ -3,30 +3,44 @@
 %
 % Compares the output of EM_pivot_calibration, specifically the position of
 % the calibration post b_post, to the given values in the THA3 PA1 output
-% files.
-%
-% Inputs:
-% file = input file name (string); this function is concerned with files
-% ending in 'empivot.txt'
-% gt_file = ground truth file name (string); this function is concerned with files
-% ending in 'output1.txt'
+% files. Only compares the first seven files, i.e., those in which a ground
+% truth output file is available.
 %
 % Output: 
 % test = a boolean; true if the calculated post position b_post matches the
 % ground truth value provided in the output file
 
-function [test] = EM_pivot_calib_test(file,gt_file)
+clear
+clc
+
+% Load all files
+empivot_files = dir(fullfile('HW3-PA1', '*empivot.txt'));
+output_files = dir(fullfile('HW3-PA1', '*output1.txt'));
+
+file = {empivot_files.name};
+gt_file = {output_files.name};
+
+for i = 1:size(gt_file,2)
 
     % Perform pivot calibration
-    [b_tip,b_post] = EM_pivot_calib(file);
+    [b_tip,b_post] = EM_pivot_calib(file{i});
     b_post = b_post'; % Transpose
 
     % Load ground truth from 'output.txt' file
-    output_file = readmatrix(gt_file,"NumHeaderLines",1);
+    output_file = readmatrix(gt_file{i},"NumHeaderLines",1);
     gt_b_post = output_file(1,:); % calbration post location from given output file
 
+    % Optional: Plot the error for each file
+    error = b_post - gt_b_post;
+    error_norm(i) = norm(error);
+    bar(error_norm)
+    xticklabels({'A','B','C','D','E','F','G'})
+    xlabel('Trial')
+    ylabel('Error')
+    grid on
+
     % Compare calculated post position to ground truth
-    tol = 0.3; % Output file is rounded to this value
+    tol = 1; % EM sensor approximate random noise, according to PA3
     assert(all(abs(b_post - gt_b_post) < tol, 'all'), "ERROR: Calculated post position does not match output file.");
 
     if all(abs(b_post - gt_b_post) < tol)
